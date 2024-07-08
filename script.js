78576b83ed030beba94f7a378df457a5cf9eea60
Alessandro Potenza <ap.alepot@gmail.com>
2024-07-08 18:43:41 +0200

    pubblicato

--- script.js
+++ script.js
@@ -1,34 +1,14 @@
-        // onMove: function(move)
-
-        // onMoveEnd: function(move)
-
-        // onChange: function(fen)
-
-        // onDragStart: function(from, piece)
-
-        // onDragMove: function(from, to, piece)
-
-        // onDrop: function(from, to, piece)
-
-        // onSnapbackEnd: function(from, piece)
+import { Chessboard } from './chessboard.js';
 
 let fen = 'r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1p3PPP/R5K1 b - - 0 19'
 
-var config = new ChessboardConfig({
-    mode: 'creative',
+var board = new Chessboard({
+    mode: 'normal',
     id_div: 'board', 
     position: 'start', 
     color: 'w', 
-    hints: true, 
-    movableColors: false,
-    moveHighlight: true,
-    draggable: true,
-    movable: true,
-    dropOffBoard: 'trash',
-    onlyLegalMoves: false,
 });
 
-var board = config.build();
 
+board.position(fen, 'b');
 
-board.setPosition(fen, 'b');
